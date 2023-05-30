4f99161b948765116a1ef4247b89843cdaeaad3b
kothaveni <kvjkrishnaveni6@gmail.com>
2023-05-30 19:39:54 +0530

    DRAFT SPRING BOOT APPLICATION.

---
+++ src/main/java/com/example/spring/practice/springbootpracticeexample/SpringBootPracticeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.practice.springbootpracticeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/practice/springbootpracticeexample/SpringBootPracticeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.practice.springbootpracticeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
